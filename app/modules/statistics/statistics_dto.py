df35a508155742e800201fe993a7c66efb471e97
Akram <a.tashpulatov@amanbay.tech>
2025-09-23 17:03:51 +0500

    statistics

--- app/modules/statistics/statistics_dto.py
+++ app/modules/statistics/statistics_dto.py
@@ -1,12 +1,13 @@
 
 from datetime import datetime
+from typing import List
 import uuid
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class StatisticsBase(BaseModel):
   device_id: uuid.UUID
-  content_id: uuid.UUID
+  ad_id: uuid.UUID
   watched_full: bool = False
 
 class StatisticsCreate(StatisticsBase):
@@ -18,3 +19,9 @@ class StatisticsPublic(StatisticsBase):
 
   class Config:
       from_attributes = True
+
+class AggregatedStatistics(BaseModel):
+    unique_devices: int = Field(..., description="Количество уникальных устройств")
+    watched_distribution: dict[str, float] = Field(..., description="Распределение досмотрели / не досмотрели")
+    daily_views: List[dict] = Field(..., description="Динамика просмотров по дням")
+    total_views: int = Field(..., description="Общее количество просмотров")
